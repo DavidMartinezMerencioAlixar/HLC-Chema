47bd98955fa5e50e2072934df8101fbf47683d20
DavidMartinezMerencioAlixar <alum.dmartinezm.2022@iesalixar.org>
2023-01-12 20:41:15 +0100

    Pagina blog creada

---
+++ blog/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def saludo(request):
+    return HttpResponse("Esta la primera página del blog")
+
+def despedida(request):
+    return HttpResponse("Esta es la página de despedida")
+
