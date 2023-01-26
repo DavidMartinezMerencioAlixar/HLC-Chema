e6cb7527bf11d8007309bfa6b8d3ebaeeef882de
DavidMartinezMerencioAlixar <alum.dmartinezm.2022@iesalixar.org>
2023-01-26 20:42:56 +0100

    BD en Django

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+class Clientes(models.Model):
+    nombre = models.CharField(max_length=50)
+    direccion = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    telefono = models.CharField(max_length=20)
+
+    def __str__(self):
+        print(self)
+
+class Articulos(models.Model):
+    nombre = models.CharField(max_length=50)
+    seccion = models.CharField(max_length=10)
+    precio = models.CharField(max_length=10)
+
+    def __str__(self):
+        print(self)
+
+class Pedidos(models.Model):
+    numero = models.CharField(max_length=10)
+    fecha = models.CharField(max_length=20)
+    entregado = models.CharField(max_length=1)
+
+    def __str__(self):
+        print(self)
+        
