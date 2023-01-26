ab9c1e6a2b1794a4a69a933eb483704a90c0e681
DavidMartinezMerencioAlixar <alum.dmartinezm.2022@iesalixar.org>
2023-01-26 21:13:22 +0100

    Creación BD Django y SQLite

--- blog/models.py
+++ blog/models.py
@@ -2,27 +2,27 @@ from django.db import models
 
 # Create your models here.
 class Clientes(models.Model):
-    nombre = models.CharField(max_length=50)
-    direccion = models.CharField(max_length=50)
-    email = models.CharField(max_length=50)
-    telefono = models.CharField(max_length=20)
+    nombre = models.CharField(max_length=50, verbose_name="Nombre completo del cliente")
+    direccion = models.CharField(max_length=50, blank=True, null=True)
+    email = models.EmailField(max_length=50)
+    telefono = models.CharField(max_length=7)
+    movil = models.CharField(max_length=9, null=True)
 
     def __str__(self):
-        print(self)
+        return self.nombre
 
 class Articulos(models.Model):
     nombre = models.CharField(max_length=50)
     seccion = models.CharField(max_length=10)
-    precio = models.CharField(max_length=10)
+    precio = models.FloatField()
 
     def __str__(self):
-        print(self)
+        return '{} y su precio es {}'.format(self.nombre, self.precio)
 
 class Pedidos(models.Model):
-    numero = models.CharField(max_length=10)
-    fecha = models.CharField(max_length=20)
-    entregado = models.CharField(max_length=1)
+    numero = models.IntegerField()
+    fecha = models.DateField()
+    entregado = models.BooleanField()
 
     def __str__(self):
-        print(self)
-        
+        return "Pedido {} entregado: {}".format(self.numero, self.entregado)
